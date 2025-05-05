ba96d4aa318c57a87e78002cf2c02d642d15f631
Neha-vis <nehacollege12@gmail.com>
2025-05-05 16:52:05 +0530

    some changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,4 @@
-// Services Swiper
-const serviceSwiper = new Swiper('.service-swiper', {
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    }
-  }
-});
+
 
 // Testimonial Swiper
 const testimonialSwiper = new Swiper(".mySwiper", {
