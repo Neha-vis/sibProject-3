fd4ba595d20e5f4a15945d523ae66ce4a0921f4a
Neha-vis <nehacollege12@gmail.com>
2025-05-05 15:56:39 +0530

    ui-chnages

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,46 @@
-const swiper = new Swiper('.service-swiper', {
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+// Services Swiper
+const serviceSwiper = new Swiper('.service-swiper', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      }
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     }
-  });
+  }
+});
+
+// Testimonial Swiper
+const testimonialSwiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+});
+
+
+
